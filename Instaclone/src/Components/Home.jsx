4940265659a2751f014947564deea720789d287d
pnkkumar123 <pankajkanwar420@gmail.com>
2024-02-28 16:02:07 +0530

    created home component

--- Instaclone/src/Components/Home.jsx
+++ Instaclone/src/Components/Home.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineFavoriteBorder,MdOutlineMood } from "react-icons/md";
+import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      {/* card */}
+      <div className="card">
+        {/* card header */}
+        <div className="card-header">
+          <div className="card-pic">
+            <img
+              src="https://images.unsplash.com/photo-1639747280804-dd2d6b3d88ac?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </div>
+          <h5>samesh</h5>
+        </div>
+        {/* card image */}
+        <div className="card-image">
+          <img
+            src="https://media.istockphoto.com/id/1336512835/photo/volcanic-eruption-in-iceland.jpg?s=612x612&w=is&k=20&c=205Aw3G46MBotlwGLKOt5ZJG8mrPpRm2A6WnR23ty5Q="
+            alt=""
+          />
+        </div>
+        {/* card-content */}
+        <div className="card-content">
+        <MdOutlineFavoriteBorder />
+        <p>1 Like</p>
+        <p className="caption">This is amazing</p>
+        </div>
+        {/* add comment */}
+        <div className="add-comment">
+        <MdOutlineMood />
+        <input type="text" name="" placeholder="add a comment" id="" />
+        <button className="post">Post</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
